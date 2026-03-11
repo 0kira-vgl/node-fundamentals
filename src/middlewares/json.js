1880f490c7e728245e27d68301672922f7dc264e
matheus <matheusgtiburcio@gmail.com>
2026-03-11 13:47:19 -0300

    Add middleware to process JSON in requests

---
+++ src/middlewares/json.js
@@ -0,0 +1,17 @@
+// middleware: interceptador de requisições
+
+export async function json(req, res) {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  res.setHeader("Content-Type", "application/json");
+}
